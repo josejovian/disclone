bfa3636605f5dbe8a871920835a4b4605edd90c3
Jose Jovian <josejovian55@gmail.com>
2022-02-22 12:02:41 +0700

    removed unused function used for debugging

--- src/utility/Validation.js
+++ src/utility/Validation.js
@@ -6,17 +6,6 @@ import {
 } from "@chakra-ui/react";
 import { Field } from "formik";
 
-/* Source:
- * https://flexiple.com/javascript-array-equality/
- */
-
-export function arrayEquals(a, b) {
-    return Array.isArray(a) &&
-        Array.isArray(b) &&
-        a.length === b.length &&
-        a.every((val, index) => val === b[index]);
-}
-
 const validateValue = (rule, name, value) => {
 	let error = '';
 
